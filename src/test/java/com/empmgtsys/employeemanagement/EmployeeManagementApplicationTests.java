0e85f79d314f575811bb7c5725252139ba31df0e
jones-martin <jonesmartin.avm@gmail.com>
2026-06-29 23:16:12 +0530

    "Database Setup and Entities Creations"

---
+++ src/test/java/com/empmgtsys/employeemanagement/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empmgtsys.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
